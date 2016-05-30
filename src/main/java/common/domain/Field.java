ad16c2467de3270ff94afe36545ef1b2f5d7fe0d
aljutik <aljutik40@gmail.com>
2016-05-30 10:34:49 +0300

    Add position for  field.

--- src/main/java/common/domain/Field.java
+++ src/main/java/common/domain/Field.java
@@ -1,9 +1,26 @@
 package common.domain;
 
 public class Field {
-    FieldMarks mark;
+    private int positionX;
+
+    private int positionY;
+
+    private FieldMarks mark;
 
     public Field() {
+    }
+
+    public Field(int positionX, int positionY) {
+        this.positionX = positionX;
+        this.positionY = positionY;
+    }
+
+    public int getPositionX() {
+        return positionX;
+    }
+
+    public int getPositionY() {
+        return positionY;
     }
 
     public FieldMarks getMark() {
